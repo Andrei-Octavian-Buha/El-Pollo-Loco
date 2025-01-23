629c48b4d218689f2a835cf95ad0f6cdd6b07a22
Andrei Buha <119259186+Andrei-Octavian-Buha@users.noreply.github.com>
2025-01-23 14:42:38 +0100

    work on pollo loco

---
+++ models/movable-object.class.js
@@ -0,0 +1,38 @@
+class MovableObject {
+  x = 120;
+  y = 300;
+  img;
+  height = 150;
+  width = 100;
+  imageCache = [];
+  currentImage = 0;
+  speed = 0.15;
+  otherDirection = false;
+
+  loadImage(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
+
+  /**
+   *
+   * @param {Array} arr - ['img/image.png ', 'img/image2.png']
+   */
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = img;
+    });
+  }
+
+  moveRight() {
+    console.log("Moving right");
+  }
+
+  moveToLeft() {
+    setInterval(() => {
+      this.x -= this.speed;
+    }, 1000 / 60);
+  }
+}
